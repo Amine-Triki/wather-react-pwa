fd1340b83aa1cb5dda834d4aa55f04b075a39850
Amine-Triki <amine.triki15@gmail.com>
2023-11-21 19:51:30 +0100

    end

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { HelmetProvider } from 'react-helmet-async';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
@@ -10,3 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </HelmetProvider>
   </React.StrictMode>,
 )
+
+serviceWorkerRegistration.register();
