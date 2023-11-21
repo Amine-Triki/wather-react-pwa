2eb0e7b7680371757708dce0bcf292cc42ce04f6
Amine-Triki <amine.triki15@gmail.com>
2023-11-21 21:02:05 +0100

    adding build

--- build/registerSW.js
+++ build/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://amine-triki.github.io/wather-react-pwa/sw.js', { scope: 'https://amine-triki.github.io/wather-react-pwa/' })})}
