ad09f139d3fedbcef8d7d305db849d228b1fa7db
Amine-Triki <amine.triki15@gmail.com>
2023-12-19 13:15:28 +0100

    change purpose image 256

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ const manifestForPlugin = {
         src: "/icon-256x256.png",
         sizes: "256x256",
         type: "image/png",
-        purpose: "icon 256 256",
+        purpose: "icon",
       },
       {
         src: "/icon-384x384.png",
