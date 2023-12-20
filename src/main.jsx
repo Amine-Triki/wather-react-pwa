0d719aaaeb1d0cae967cdd2e8909a395e322934f
Amine-Triki <amine.triki15@gmail.com>
2023-12-20 13:57:42 +0100

    add vite-env.d.ts & edit main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import { HelmetProvider } from 'react-helmet-async';
+import { useRegisterSW } from 'virtual:pwa-register/react';  // استيراد useRegisterSW
 
+const MyRootComponent = () => {
+  const { needRefresh, offlineReady, updateServiceWorker } = useRegisterSW();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HelmetProvider>
-    <App />
-    </HelmetProvider>
-  </React.StrictMode>,
-)
+  useEffect(() => {
+    updateServiceWorker();
+  }, []);
 
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </React.StrictMode>
+  );
+};
 
-
-
-
+export default MyRootComponent;  
 
--- vite.config.js
+++ vite.config.js
@@ -12,14 +12,34 @@ const manifestForPlugin = {
     description: "An app that can show the weather forecast for your city.",
     icons: [
       
-      
-      
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      },
+      {
+        src: "/icon-144x144.png",
+        sizes: "144x144",
+        type: "image/png",
+        purpose: "any maskable"
+      },
       {
         src: "/icon-256x256.png",
         sizes: "256x256",
         type: "image/png",
-        purpose: "icon",
+        purpose: "icon 256 256"
       },
+      {
+        src: "/icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+        purpose: "any maskable"
+      }
       
     ],
     theme_color: "#171717",
