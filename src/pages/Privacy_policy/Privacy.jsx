142f83a7b35af886484316b9c7a424d319745635
Amine-Triki <amine.triki15@gmail.com>
2025-02-16 22:14:52 +0100

    adding Privacy Policy and Terms of Service pages

---
+++ src/pages/Privacy_policy/Privacy.jsx
@@ -0,0 +1,19 @@
+import './Privacy.css'
+
+import { Helmet } from 'react-helmet-async';
+
+const Privacy = () => {
+
+  return (
+    
+    <main>
+      <Helmet>
+        <title>Privacy Policy</title>
+      </Helmet> 
+
+    </main>
+    
+  )
+}
+
+export default Privacy
---
+++ src/pages/Terms_of_service/Terms.jsx
@@ -0,0 +1,19 @@
+import './Contact.css'
+
+import { Helmet } from 'react-helmet-async';
+
+const Contact = () => {
+
+  return (
+    
+    <main>
+      <Helmet>
+        <title>Terms of Service</title>
+      </Helmet> 
+
+    </main>
+    
+  )
+}
+
+export default Contact
