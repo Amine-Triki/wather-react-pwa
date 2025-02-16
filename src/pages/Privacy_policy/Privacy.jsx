4dbb5ac7ed9e9efee117872389a5f145dcd07119
Amine-Triki <amine.triki15@gmail.com>
2025-02-16 23:00:49 +0100

    edit Privacy Policy and Terms of Service pages

--- src/pages/Privacy_policy/Privacy.jsx
+++ src/pages/Privacy_policy/Privacy.jsx
@@ -6,10 +6,50 @@ const Privacy = () => {
 
   return (
     
-    <main>
+    <main className='d-flex justify-content-center'>
       <Helmet>
         <title>Privacy Policy</title>
       </Helmet> 
+
+      <div className='terms d-flex flex-column p-3 justify-content-center align-items-center'>
+        <p >
+        Your privacy is important to us. This policy explains how we collect, use, and protect your data when you use aminetriki site.
+       </p>
+<b>1. Data We Collect </b>
+<p>
+Non-personal data such as IP addresses, browser type, pages visited.
+
+Cookies to enhance your experience.
+</p>
+
+<b>2. How We Use Your Data</b>
+<p>
+To improve our services.
+
+To display relevant ads through Google Ads and other companies.
+</p>
+<b>3. Data Sharing</b>
+<p>
+We share your data only with service providers such as Google Ads.
+</p>
+<b>4. Cookies</b>
+<p>
+We use cookies to collect data. You can disable cookies from your browser settings.
+</p>
+
+<b>5. Security</b>
+<p>
+We take security measures to protect your data, but no system is 100% secure.
+        </p>
+<b>6. Changes</b>
+<p>
+We may update this privacy policy from time to time.
+        </p>
+<b>7. Contact Us</b>
+<p>
+For any privacy-related inquiries, contact us at [contact@aminetriki.com.tn].
+        </p>
+      </div>
 
     </main>
     
--- src/pages/Terms_of_service/Terms.jsx
+++ src/pages/Terms_of_service/Terms.jsx
@@ -6,10 +6,44 @@ const Terms = () => {
 
   return (
     
-    <main>
+    <main className='d-flex justify-content-center'>
       <Helmet>
         <title>Terms of Service</title>
       </Helmet> 
+      <div className='terms d-flex flex-column p-3 justify-content-center align-items-center'>
+        <p >
+        Welcome to aminetriki site
+
+By using this website, you agree to comply with these terms and conditions. If you do not agree to any of these terms, please do not use the website.
+</p>
+<b>1. Use of the Website </b>
+<p>
+You must not use the website for any illegal or prohibited purposes.
+
+You may not attempt to hack the website or exploit any security vulnerabilities.
+
+The site administration reserves the right to modify or discontinue any service at any time.
+</p>
+
+<b>2. Content and Services</b>
+<p>
+The website and services are provided “as is” without any warranties.
+
+We are not responsible for any damages resulting from the use of the provided tools or services.
+</p>
+<b>3. Changes to Terms</b>
+<p>
+These terms may be modified at any time, and updates will be posted on this page.
+</p>
+<b>4. Governing Laws</b>
+<p>
+These terms are governed by the laws of the European Union, and any disputes shall be settled through the competent courts.
+</p>
+<b>5. Contact Us </b>
+<p>
+If you have any questions, please contact us at [contact@aminetriki.com.tn].
+        </p>
+      </div>
 
     </main>
     
