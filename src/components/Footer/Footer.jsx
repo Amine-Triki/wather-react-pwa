a2662e2698718f70951551695a3ef6d04c124185
Amine-Triki <amine.triki15@gmail.com>
2025-03-02 22:27:53 +0100

    change nav

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import './Footer.css'
-
+import { Link } from "react-router-dom";
 let year = new Date().getFullYear() ;
 
 const Footer = () => {
@@ -7,7 +7,7 @@ const Footer = () => {
     
     <>
       <footer className="footer py-3 "style={{ backgroundColor: '#ffc0cb' }}  >
-        <p id="Rights" className="container text-center mb-0">2023 -- {`${year} © Amine Triki || All Rights Reserved`} </p>
+        <p id="Rights" className="container text-center mb-0">2023 -- {`${year} © Amine Triki || All Rights Reserved`} ||<span> <Link to="/terms_of_service" className='text-dark'>Terms of Service</Link>  <Link to="/privacy_policy" className='text-dark' >Privacy Policy</Link></span> </p>
     </footer>
     </>
   )
